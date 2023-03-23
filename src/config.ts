f79aeff3b9fdac56bb44c93b48c1e9b836660721
cuongvu3009 <cuongvu3009@gmail.com>
2023-03-23 16:12:08 +0200

    remove unused key

--- src/config.ts
+++ src/config.ts
@@ -9,13 +9,10 @@ class Config {
   public SECRET_KEY_TWO: string | undefined;
   public CLIENT_URL: string | undefined;
   public SERVER_PORT: string | undefined;
-  public REDIS_HOST: string | undefined;
-  public REDIS_PASSWORD: string | undefined;
-  public REDIS_PORT: string | undefined;
 
   private readonly DEFAULT_DATABASE_URL =
     'mongodb+srv://cuong:1234@nodeexpressproject.nlzou.mongodb.net/Kesko-Exercise?retryWrites=true&w=majority';
-  private readonly DEFAULT_NODE_ENV = 'development';
+
   private readonly DEFAULT_CLIENT_URL = 'http://localhost:3000';
 
   constructor() {
@@ -24,9 +21,6 @@ class Config {
     this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || '';
     this.CLIENT_URL = process.env.CLIENT_URL || this.DEFAULT_CLIENT_URL;
     this.SERVER_PORT = process.env.SERVER_PORT || '';
-    this.REDIS_HOST = process.env.REDIS_HOST || '';
-    this.REDIS_PASSWORD = process.env.REDIS_PASSWORD || '';
-    this.REDIS_PORT = process.env.REDIS_PORT || '';
   }
 
   //	logger
