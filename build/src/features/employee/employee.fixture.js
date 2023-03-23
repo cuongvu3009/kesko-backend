1605fb2bf4d91b5f10d6a7b9342f95db64dc71f7
cuongvu3009 <cuongvu3009@gmail.com>
2023-03-23 15:11:25 +0200

    del build

--- build/src/features/employee/employee.fixture.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Employee1 = void 0;
-const employee_schema_1 = require("./employee.schema");
-exports.Employee1 = new employee_schema_1.EmployeeModel({
-    EmployeeID: 5
-});
-//# sourceMappingURL=employee.fixture.js.map
